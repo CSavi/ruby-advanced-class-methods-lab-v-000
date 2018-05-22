f85df7c4deb36a90d214c5062e3e1ddfde52658e
Carrie Savinell <carrielane89@gmail.com>
2018-05-22 15:37:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
   
   def self.new_from_filename(file_name)
     s = self.new 
-    s.name = file_name.split(/[^a-zA-Z\s]|\s/)
+    s.name = file_name.split(/[^a-zA-Z\s]|\s/)[1])
     s.artist_name = file_name.split(/^/)
     s 
   end 
