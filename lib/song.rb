d4c385e884204b177c1c31270f52ba6f7db8dc13
Carrie Savinell <carrielane89@gmail.com>
2018-05-22 15:25:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   def self.new_from_filename(file_name)
     s = self.new 
     s.name = file_name.split("-").slice!
-    s.artist_name(".mp3").join(" ")
+    s.artist_name = (".mp3").join(" ")
   end 
   
   
